0f6d45fc6691da1b10d7c80b9b6521e3e7978615
Anteste <reversodr@gmail.com>
2019-09-21 09:13:57 +0100

    feat: frontend

---
+++ src/Controller/AdminPropertyController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Property;
+use App\Form\PropertyType;
+use App\Repository\PropertyRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AdminPropertyController extends AbstractController
+{
+
+    /**
+     * @var PropertyRepository
+     */
+    private $repository;
+    /**
+     * @var \Doctrine\Common\Persistence\ObjectManager
+     */
+    private $em;
+
+    public function __construct(PropertyRepository $repository, ObjectManager $em)
+    {
+        $this->repository = $repository;
+        $this->em = $em;
+    }
+
+    /**
+     * @Route("/admin", name="admin.property.index")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function index()
+    {
+        $properties = $this->repository->findAll();
+        return $this->render('admin/property/index.html.twig', compact('properties'));
+    }
+
+    /**
+     * @Route("/admin/property/create", name="admin.property.new")
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function new(Request $request)
+    {
+        $property = new Property();
+        $form = $this->createForm(PropertyType::class, $property);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $this->em->persist($property);
+            $this->em->flush();
+            $this->addFlash('success', 'Bien crée avec succès');
+            return $this->redirectToRoute('admin.property.index');
+        }
+
+        return $this->render('admin/property/new.html.twig',[
+            'property' => $property,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route ("/admin/property/{id}", name="admin.property.edit", methods="GET|POST")
+     * @param \App\Entity\Property $property
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function edit(Property $property, Request $request)
+    {
+        $form = $this->createForm(PropertyType::class, $property);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $this->em->flush();
+            $this->addFlash('success', 'Bien modifié avec succès');
+            return $this->redirectToRoute('admin.property.index');
+        }
+
+        return $this->render('admin/property/edit.html.twig',[
+            'property' => $property,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/admin/property/{id}", name="admin.property.delete", methods= "DELETE")
+     * @param \App\Entity\Property $property
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function delete(Property $property, Request $request)
+    {
+        if ($this->isCsrfTokenValid('delete' . $property->getId(), $request->get('_token')))
+        {
+            $this->em->remove($property);
+            $this->em->flush();
+            $this->addFlash('success', 'Bien supprimé avec succès');
+        }
+        return $this->redirectToRoute('admin.property.index');
+    }
+
+}
+
---
+++ src/Form/PropertyType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Property;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PropertyType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('surface')
+            ->add('rooms')
+            ->add('bedrooms')
+            ->add('floor')
+            ->add('price')
+            ->add('heat', ChoiceType::class, [
+                'choices' => $this->getChoices()
+            ])
+            ->add('city')
+            ->add('adress')
+            ->add('postal_code')
+            ->add('sold')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Property::class,
+            'translation_domain' => 'forms'
+        ]);
+    }
+
+    private function getChoices()
+    {
+        $choices = Property::HEAT;
+        $output = [];
+        foreach ($choices as $k => $v)
+        {
+            $output[$v] = $k;
+        }
+        return $output;
+    }
+}
